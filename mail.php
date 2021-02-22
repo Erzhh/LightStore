8be9fd15c31738b4fdb02f4de75ca96a0cdbf595
Bekasyl1972 <bekasyl00106@icloud.com>
2021-02-22 19:44:59 +0600

    rftgyhuj

--- mail.php
+++ mail.php
@@ -4,11 +4,11 @@
     $mail = "erzhigit1407@gmail.com";
 
 
-    $headers .= "X-PHP-Originating-Script: 0:1.php";
-    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
-    $headers .= 'From: Myarena.com  <bekasyl00106@icloud.com>' . "\r\n";
+    // $headers .= "X-PHP-Originating-Script: 0:1.php";
+    // $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+    $headers = 'From: bekasyl00106@icloud.com';
 
-    $m = mail($mail, 'Код подтверждений',$message, $headers);
+    $m = mail($mail, 'Код подтверждений',"X-PHP-Originating-Script: 0:1.php", $headers);
 
      if($m) {
 		echo "The email message was sent.";
