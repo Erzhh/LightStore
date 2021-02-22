a44a8d5c9512f842dd715179fd6d7b983807d043
Bekasyl1972 <bekasyl00106@icloud.com>
2021-02-22 19:19:29 +0600

    cdd

--- mail.php
+++ mail.php
@@ -6,7 +6,7 @@
 
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
-    $headers .= 'From: Myarena.kz  <bekasyl00106@icloud.com>' . "\r\n";
+    $headers .= 'From: Myarena.com  <bekasyl00106@icloud.com>' . "\r\n";
 
     $m = mail($mail, 'Код подтверждений',$message, $headers);
 
