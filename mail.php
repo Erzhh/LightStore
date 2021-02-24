375c2009c03faa2219acbbb83daf723cf48b32bf
MAFIOZNIK007 <danonet61@mail.ru>
2021-02-24 17:37:38 +0600

    да

--- mail.php
+++ mail.php
@@ -1,12 +1,12 @@
 <?php 
 
     $message =  "Hello";
-    $mail = "erzhigit1407@gmail.com";
+    $mail = "nikitamatveev040@gmail.com";
 
 
-    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "X-PHP-Origination-Script: 0:1.php";
     $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
-    $headers .= 'From: Your@gmail.com' . "\r\n";
+    $headers .= 'From: paamp89@mail.ru' . "\r\n";
 
     $m = mail($mail, 'Код подтверждений',$message, $headers);
 
