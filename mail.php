4006ec2437239e6170b38ce3e6e46805c2046e72
ernarg <github17112005>
2021-02-24 18:16:50 +0600

    ernar change

--- mail.php
+++ mail.php
@@ -17,11 +17,11 @@
     // }
 
     require_once "setting/SMTP.php";
-    $mailSMTP = new SendMailSmtpClass('yourmail@gmail.com', 'password', 'ssl://smtp.gmail.com', 'From', 465);
+    $mailSMTP = new SendMailSmtpClass('github17112005@gmail.com', 'era17112005', 'ssl://smtp.gmail.com', 'From', 465);
     $headers= "MIME-Version: 1.0\r\n";
     $headers .= "Content-type: text/html; charset=utf-8\r\n";
-    $headers .= "From: From<yourmail@gmail.com>\r\n";
-    $result =  $mailSMTP->send('yourmail@gmail.com', 'Subject', "Text", $headers);
+    $headers .= "From:github17112005@gmail.com From<@gmail.com>\r\n";
+    $result =  $mailSMTP->send('github17112005@gmail.com', 'Subject', "Text", $headers);
 
 
      ?>
