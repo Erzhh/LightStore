d2e47964b5eaad730060b69fcfbc401129a9a675
MAFIOZNIK007 <danonet61@mail.ru>
2021-03-03 19:05:45 +0600

    da

--- admin/includesforadmin/headforadmin.php
+++ admin/includesforadmin/headforadmin.php
@@ -3,13 +3,13 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="description" content="Responsive Bootstrap 4 Admin &amp; Dashboard Template">
 	<meta name="author" content="Bootlab">
-    <base href="https://appstack.bootlab.io">
 	<title>Default Dashboard | AppStack - Admin &amp; Dashboard Template</title>
-	<link rel="stylesheet" href="assets/css/cssforadmin.css">
-	<link rel="stylesheet" href="assets/css/form.css">
+	<link rel="stylesheet" href="../assets/css/cssforadmin.css">
+	<link rel="stylesheet" href="../assets/css/form.css">
+	<link rel="stylesheet" href="../assets/css/main.css">
 	<link rel="canonical" href="https://appstack.bootlab.io/dashboard-default.html">
 	<link rel="shortcut icon" href="img/favicon.ico">
 
 	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&amp;display=swap" rel="stylesheet">
-	<link class="js-stylesheet" href="css/light.css" rel="stylesheet">
+	<link class="js-stylesheet" href="https://appstack.bootlab.io/css/light.css" rel="stylesheet">
 	<script src="js/settings.js"></script>
