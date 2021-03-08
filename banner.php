12c9168c258dba13d02253fa08079b927f45e78a
ernarg <github17112005>
2021-03-08 19:32:40 +0600

    vbcvb

--- banner.php
+++ banner.php
@@ -1,15 +1,13 @@
 <?php
 $connect=mysqli_connect('localhost','root','root','lightstore');
-$query=mysqli_query($connect,"SELECT * FROM `banner` WHERE `id`='$_GET[news_id]'");
+$query=mysqli_query($connect,"SELECT * FROM `banner`");
 $banner=mysqli_fetch_assoc($query);
-	$c = mysqli_connect('localhost', 'root', 'root', 'it start');         
-
+	$c = mysqli_connect('localhost', 'root', 'root', 'lightstore');         
+	$query=mysqli_query($c, "SELECT *FROM `banner`");
     $update = mysqli_query($c, "UPDATE  `banner` SET `btn_text_ru`='$_POST[title]', `btn_text_kz`='$_POST[titlekz]', `des_ru`='$_POST[desk]',`image`='$_POST[img]', `des_kz`='$_POST[deskkz]', WHERE `id`='$_GET[news_id]'");
     
-	if($update){
- header("Location:banner.php");
+	
 
-	}
 ?>
 <!DOCTYPE html>
 <html>
@@ -23,18 +21,30 @@ $banner=mysqli_fetch_assoc($query);
 
 
 	<form method="POST" enctype="multipart/form-data">
-		<div class="name">
+	<div class="namen">
 			<div class="fn">
 				<label for="tit">Заголовок</label>
 				<br>
-				<textarea type="text" name="title" id="tit" class="n"><?=$banner['btn_text_ru']?></textarea>
+				<textarea type="text" name="title" id="tit" class="n"><?=$banner['title']?></textarea>
+				</div>
+
+				<div class="fn">
+				<label for="tit">Заголовок кз</label>
+				<br>
+				<textarea type="text" name="titlekz" id="tit" class="n"><?=$banner['titlekz']?></textarea>
+				</div>
+		<div class="namen">
+			<div class="fn">
+				<label for="tit">кнопка</label>
+				<br>
+				<textarea type="text" name="btnru" id="tit" class="n"><?=$banner['btn_text_ru']?></textarea>
               
 			</div>  
             <div class="name">
 			<div class="fn">
-				<label for="tit">Заголовок</label>
+				<label for="tit">кнопка кз</label>
 				<br>
-				<textarea type="text" name="titlekz" id="titt" class="nj" value="<?=$banner['btn_text_kz']?>"></textarea>
+				<textarea type="text" name="btnkz" id="titt" class="nj" value="<?=$banner['btn_text_kz']?>"></textarea>
               
 			</div>  
             
@@ -56,7 +66,7 @@ $banner=mysqli_fetch_assoc($query);
 		</div>
         <div class="dis">
 
-			<label for="dis">Описание</label>
+			<label for="dis">Описание кз</label>
 			<br>
 			<textarea type="text" name="deskkz" id="dis"><?=$banner['des_kz']?></textarea>
 		</div>
@@ -69,12 +79,13 @@ $banner=mysqli_fetch_assoc($query);
 
 
 	<?php
+	$query=mysqli_query($c, "SELECT *FROM `banner`");
+while($banner=mysqli_fetch_assoc($query)){
+	
 
-       
-		if(isset($_POST['title'])){
-        	$c = mysqli_connect('localhost', 'root', 'root', 'lightstore');
-		
-
+	if(isset($_POST['title'])){
+		$c = mysqli_connect('localhost', 'root', 'root', 'lightstore');
+	
 			$old_name=$_FILES['img']['name'];
 			$format=end(explode('.',$old_name));
 			$new_name=mt_rand(3000,10000).time().'.'.$format;
@@ -82,21 +93,37 @@ $banner=mysqli_fetch_assoc($query);
 			$upload=move_uploaded_file(
 									  $_FILES['img']['tmp_name'],
 									  'assets/img/'.$new_name
-			);
+			);	
 			
-		
-	$insert = mysqli_query($c, "INSERT INTO `banner` (`btn_text_ru`,`btn_text_kz`, `des_ru`, `des_kz`, `image` ) VALUES ('$_POST[title]', '$_POST[titlekz]', '$new_name', '$_POST[desk]', '$_POST[deskkz]','$_POST[img]')");
-		
-	if($insert){
-	echo "news";
-	}
-	else{
-		echo "INSERT INTO `banner`(`btn_text_ru`, `btn_text_kz`, `desk`, `deskkz`, `image`) VALUES ('$_POST[titleNews]', '$_POST[textNews]', '$new_name', '$_POST[authorNews]', '$_POST[dateOfNews]')";
+$insert = mysqli_query($c, "INSERT INTO `banner` (`title`,`titlekz`,`des_ru`, `des_kz`,  `btn_text_ru`,`btn_text_kz`, `image` ) VALUES ('$_POST[title]', '$_POST[titlekz]', '$_POST[desk]', '$new_name', '$_POST[deskkz]', 
+'$_POST[btnru]','$_POST[btnkz]',    '$new_name' ");	
+// if($insert){
+// echo "news";
+// }
+// else{
+// 	echo "INSERT INTO `banner`(`btn_text_ru`, `btn_text_kz`, `des_ru`, `des_kz`, `image`) VALUES ('$_POST[title]', '$_POST[titlekz]', '$new_name', '$_POST[desk]', '$_POST[deskkz]')";
+
+			echo "<div class='card' style='width: 18rem;'>
+<img src='assets/img/$banner[image]' class='card-img-top'>
+<div class='card-body'>
+  <p class='card-text'>
+   $banner[title]
+  </p>	 
+    
+</div>
+</div>";
 	}
 
-} 
-		 
-    
-	?>
+	}
+
+?>	
+
+
+
+
+
+
+
+
 </body>
 </html>
